78eb097435376cffb8df400df960fdfd2849a1fb
CANOLIO <128939087+CANOLIO@users.noreply.github.com>
2023-03-28 22:42:59 -0300

    Update Código.py
    fixed the problem regarding inconsistent responses generated by the bot, also added an entry to enter the api_key in a more user-friendly way

--- Código.py
+++ Código.py
@@ -31,8 +31,10 @@ def load_pdf(file_path):
         return raw_text
 
 def search():
+    #conseguir la api_key
+    api_key = api_key_entry.get()
     # load the embeddings
-    embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
+    embeddings = OpenAIEmbeddings(openai_api_key=api_key)
 
     # create the document search engine
     file_path = pdf_entry.get()
@@ -47,15 +49,15 @@ def search():
     docsearch = FAISS.from_texts(texts, embeddings)
 
     # load the question-answering chain
-    chain = load_qa_chain(OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"), temperature=0.3), chain_type="stuff")
+    chain = load_qa_chain(OpenAI(openai_api_key = api_key, temperature=0.3), chain_type="stuff")
 
     # perform the search
     query = query_entry.get()
-    prompt = ""
+    prompt = "Responda a la pregunta basándose en el siguiente texto. Las respuestas deben ser elaboradas y basarse únicamente en el contexto. Comenta dónde se encuentra en el texto la información usada para responder. Elimina toda información que sea irrelevante para la respuesta"
     query_with_prompt = prompt + query # se agrega el prompt al query
     docs = docsearch.similarity_search(query_with_prompt)
     result_text.delete('1.0', tk.END)
-    result_text.insert(tk.END, docs[0].page_content)
+
     answer = chain.run(input_documents=docs, question=query_with_prompt)
     result_text.insert(tk.END, answer)
 
@@ -71,10 +73,14 @@ pdf_button = tk.Button(window, text="Browse", command=lambda: browse_pdf(pdf_ent
 query_label = tk.Label(window, text="Enter your query:")
 query_entry = tk.Entry(window, width=50)
 search_button = tk.Button(window, text="Search", command=search)
-
+api_key_label = tk.Label(window, text="Enter your OpenAI API key:")
+api_key_entry = tk.Entry(window, width=50)
 result_label = tk.Label(window, text="Results:")
 result_text = tk.Text(window, height=15)
+
 # place the widgets in the window
+api_key_entry.pack()
+api_key_label.pack()
 pdf_label.pack()
 pdf_entry.pack()
 pdf_button.pack()
@@ -84,8 +90,9 @@ search_button.pack()
 result_label.pack()
 result_text.pack()
 
+
 # configure the button to call the search function
-search_button.configure(command=search)
+search_button = tk.Button(window, text="Search", command=lambda: search(api_key_entry.get()))
 
 # run the main loop of the GUI
 window.mainloop()
